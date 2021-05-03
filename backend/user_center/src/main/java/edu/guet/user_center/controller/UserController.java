e68eecd6de8f918e8a28ba03a817b5a9802752b3
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2021-05-04 01:11:21 +0800

    完成 getUserInfo()

--- backend/user_center/src/main/java/edu/guet/user_center/controller/UserController.java
+++ backend/user_center/src/main/java/edu/guet/user_center/controller/UserController.java
@@ -1,16 +1,15 @@
 package edu.guet.user_center.controller;
 
 
+import edu.guet.common.JwtUtils;
 import edu.guet.common.R;
 import edu.guet.user_center.entity.User;
 import edu.guet.user_center.entity.vo.RegisterVo;
 import edu.guet.user_center.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * <p>
@@ -50,6 +49,20 @@ public class UserController {
             return R.ok();
         }else {
             return R.error().message(registerInfo);
+        }
+    }
+
+    //根据token获取用户信息
+    @GetMapping("getUserInfo")
+    public R getUserInfo(HttpServletRequest request) {
+        //调用jwt工具类的方法。根据request对象获取头信息，返回用户id
+        String userId = JwtUtils.getUserIdByJwtToken(request.getHeader("token"));
+        //查询数据库根据用户id获取用户信息
+        User user = memberService.getById(userId);
+        if (null == user){
+            return R.error();
+        }else {
+            return R.ok().data("userInfo",user);
         }
     }
 }
