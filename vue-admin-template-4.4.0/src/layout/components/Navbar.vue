433ced89920eb44bf4acac95b0e95c1beae5a7e2
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2021-05-04 10:19:27 +0800

    自定义的 Navbar
    就修改了头像下拉框部分

--- vue-admin-template-4.4.0/src/layout/components/Navbar.vue
+++ vue-admin-template-4.4.0/src/layout/components/Navbar.vue
@@ -11,18 +11,12 @@
           <i class="el-icon-caret-bottom" />
         </div>
         <el-dropdown-menu slot="dropdown" class="user-dropdown">
-          <router-link to="/">
+          <router-link to="/home">
             <el-dropdown-item>
               Home
             </el-dropdown-item>
           </router-link>
-          <a target="_blank" href="https://github.com/PanJiaChen/vue-admin-template/">
-            <el-dropdown-item>Github</el-dropdown-item>
-          </a>
-          <a target="_blank" href="https://panjiachen.github.io/vue-element-admin-site/#/">
-            <el-dropdown-item>Docs</el-dropdown-item>
-          </a>
-          <el-dropdown-item divided @click.native="logout">
+          <el-dropdown-item @click.native="logout">
             <span style="display:block;">Log Out</span>
           </el-dropdown-item>
         </el-dropdown-menu>
@@ -53,7 +47,7 @@ export default {
     },
     async logout() {
       await this.$store.dispatch('user/logout')
-      this.$router.push(`/login?redirect=${this.$route.fullPath}`)
+      this.$router.push(`/?redirect=${this.$route.fullPath}`)
     }
   }
 }
