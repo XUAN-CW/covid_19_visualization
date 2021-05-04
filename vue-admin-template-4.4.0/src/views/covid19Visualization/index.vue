1fbff9bc6f4b8e1f6da0c403a806d7fce2157d6f
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2021-05-04 11:26:52 +0800

    完成跳转到登录注册页面

--- vue-admin-template-4.4.0/src/views/covid19Visualization/index.vue
+++ vue-admin-template-4.4.0/src/views/covid19Visualization/index.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
     <h1>疫情数据可视化主页面</h1>
+    <router-link to="/login">
+      login
+    </router-link>
+    <router-link to="/register">
+      register
+    </router-link>
   </div>
 </template>
 
