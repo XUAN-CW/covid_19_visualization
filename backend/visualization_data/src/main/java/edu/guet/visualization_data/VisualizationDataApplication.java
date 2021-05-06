70232ce8cfb5182691f722e6446a643e944cad5f
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2021-05-06 09:29:55 +0800

    存档

--- backend/visualization_data/src/main/java/edu/guet/visualization_data/VisualizationDataApplication.java
+++ backend/visualization_data/src/main/java/edu/guet/visualization_data/VisualizationDataApplication.java
@@ -3,7 +3,10 @@ package edu.guet.visualization_data;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 @SpringBootApplication
+@ComponentScan(basePackages = {"edu.guet"})
 public class VisualizationDataApplication {
 
     public static void main(String[] args) {
